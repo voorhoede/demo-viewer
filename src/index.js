7d58a294fc6cd5f532f23b8a4c3f70fe7b611fc8
Jasper Moelker <j.b.moelker@activeids.nl>
2016-04-15 14:37:53 +0200

    feat(app): support initialisation via external module mapping (in JSON) (cherry picked from commit 538971a)

--- src/index.js
+++ src/index.js
@@ -6,5 +6,41 @@ window.demoViewer = function(config){
     app.innerHTML = element.innerHTML;
     element.innerHTML = '';
     element.appendChild(app);
-    window.riot.mount(app, config);
+
+    if (typeof config.modules === 'string') {
+        getJson(config.modules, function(err, modules) {
+            if (err) { console.log('error fetching modules', err); }
+            window.riot.mount(app, { modules: modules, sizes: config.sizes });
+        });
+    } else {
+        window.riot.mount(app, config);
+    }
+
+
+    /**
+     * Get JSON from URL.
+     *
+     * @param {String} url
+     * @param {Function} callback   - called with (err, json)
+     */
+    function getJson(url, callback) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                try {
+                    var json = JSON.parse(request.responseText);
+                    callback(null, json);
+                } catch (err) {
+                    callback(err);
+                }
+            } else {
+                callback(request);
+            }
+        };
+        request.onerror = function() {
+            callback(request);
+        };
+        request.send();
+    }
 };
